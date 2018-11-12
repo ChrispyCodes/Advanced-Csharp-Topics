01266ad694e689c5e9b5654e64d4c9b261041fa9
Christopher <thetoph@outlook.com>
2018-11-12 11:19:23 -0800

    first push/delegates

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            Action<Photo> filterHandler = filters.ApplyBrigthness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEye;
+
+            processor.Process("Photo.jpg", filterHandler);
+
+            Console.ReadLine();
+        }
+
+        static void RemoveRedEye(Photo photo)
+        {
+            Console.WriteLine("Apply RemoveRedEye");
+        }
+    }
+}

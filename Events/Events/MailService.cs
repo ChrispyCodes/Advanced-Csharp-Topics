a8646b9aea96dd51cfacfd58a9c90d462912089e
Christopher <thetoph@outlook.com>
2018-11-12 17:01:52 -0800

    Delegate events

---
+++ Events/Events/MailService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class MailService
+    {
+        public void OnVideoEncoded(object source, VideoEventArgs e)
+        {
+            Console.WriteLine("MailService: Sending an email..." + e.Video.Title);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Events/Events/MessageService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+        public class MessageService
+        {
+            public void OnVideoEncoded(object source, VideoEventArgs args)
+            {
+                Console.WriteLine("MessageService: Sending a text message..." + args.Video.Title);
+            Console.ReadLine();
+            }
+        }
+}
